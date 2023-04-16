dd94304a3e8dc14d881043e7469bb09cafc55076
SwiftButShaky <123132390+SwiftButShaky@users.noreply.github.com>
2023-04-16 09:42:56 -0700

    Update software-task.py

--- software-task.py
+++ software-task.py
@@ -1,5 +1,6 @@
-data = ["72366235", 2435234, False, 5.76334]
+# that last string is for you Varun
+data = ["72366235", 2435234, False, 5.76334, 'nva gona giv u up']
 
-# Code here:
-
+# normally I use i in range() but I guess I'll just go with the simple route
 for item in data:
+  print(item)
